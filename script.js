85270e7d274d44de264dc8c09ed551801938a90f
Chris Tai <christopherctai@gmail.com>
2023-02-01 16:41:38 -0800

    Add functionality to add books to library

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let myLibrary = [];
 
+let library = document.querySelector(".main-content");
+const form = document.querySelector(".form");
+const close_button = document.querySelector(".close");
+const add_book_button = document.querySelector(".add-book");
+const submit_button = document.querySelector(".form-btn");
+close_button.addEventListener("click", closeForm);
+add_book_button.addEventListener("click", openForm);
+submit_button.addEventListener("click", addBookToLibrary);
+let title;
+let author;
+let pages;
+let have_read;
+
 function Book(title, author, pages, have_read) {
   this.title = title;
   this.author = author;
@@ -7,15 +20,45 @@ function Book(title, author, pages, have_read) {
   this.have_read = Boolean(have_read);
 }
 
-let library = document.querySelector(".main-content");
-let form = document.querySelector(".form");
-let submit_form_button = document.querySelector(".form-btn");
+function addBookToLibrary(e) {
+  e.preventDefault();
+  let titleField = document.getElementById("title");
+  let authorField = document.getElementById("author");
+  let pagesField = document.getElementById("pages");
+  let haveReadField = document.getElementById("have_read");
+  if (titleField.value && authorField.value && pagesField.value) {
+    title = titleField.value;
+    author = authorField.value;
+    pages = pagesField.value;
+    have_read = haveReadField.checked;
+    let book = new Book(title, author, pages, have_read);
+    myLibrary.push(book);
+    form.style.display = "none";
+  }
+  displayLibrary();
+}
 
-submit_form_button.addEventListener("click", closeForm());
-
-/* function addBookToLibrary() {
-  let book = new Book(title, author, pages, have_read);
-} */
+function displayLibrary() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookDiv = document.createElement("div");
+    bookDiv.classList.add('book');
+    let titleDiv = document.createElement("div");
+    let authorDiv = document.createElement("div");
+    let pagesDiv = document.createElement("div");
+    let haveReadButton = document.createElement("button");
+    let removeButton = document.createElement("button");
+    titleDiv.textContent = `${book["title"]}`;
+    authorDiv.textContent = `${book["author"]}`;
+    pagesDiv.textContent = `${book["pages"]} Pages`;
+    haveReadButton.textContent = "Read";
+    removeButton.textContent = "Remove";
+    haveReadButton.classList.add("btn");
+    removeButton.classList.add("btn");
+    bookDiv.append(titleDiv, authorDiv, pagesDiv, haveReadButton, removeButton);
+    library.appendChild(bookDiv);
+  }
+}
 
 function openForm() {
   form.style.display = "flex";
